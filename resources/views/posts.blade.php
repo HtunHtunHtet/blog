b6a73df6937b3d55ff7946bb82f7ffa36cb57443
htunhtunhtet <ryanhhh91@gmail.com>
2023-07-01 15:10:01 +0800

    change blade firendly

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,21 +3,16 @@
 <title>My blog</title>
 <link rel="stylesheet" href="/app.css">
 <body>
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'margin-bottom': '' }}">
+           <h1>
+               <a href="/posts/{{ $post->slug }}">
+                   {{ $post->title }}
 
+               </a>
+           </h1>
 
-<?php foreach ($posts as $post) : ?>
-
-<article>
-   <h1>
-       <a href="/posts/<?= $post->slug; ?>">
-               <?php echo $post->title; ?>
-       </a>
-   </h1>
-
-    <div><?= $post->excerpt;?></div>
-</article>
-
-<?php endforeach; ?>
-
-
+            <div>{{ $post->excerpt }}</div>
+        </article>
+    @endforeach
 </body>
