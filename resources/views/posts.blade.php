6d3f6e8bacc4756f82eba6d5324a79a2bbe2ff7a
htunhtunhtet <ryanhhh91@gmail.com>
2023-07-08 16:28:42 +0800

    update posts and post page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,35 +1,16 @@
 <x-layout>
-    {{--    @foreach ($posts as $post)--}}
-    {{--        <article class="{{ $loop->even ? 'margin-bottom': '' }}">--}}
-    {{--            <h1>--}}
-    {{--                <a href="/posts/{{ $post->slug }}">--}}
-    {{--                    {!!  $post->title  !!}--}}
-    {{--                </a>--}}
-    {{--            </h1>--}}
-
-    {{--            <p>--}}
-    {{--                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name  }}</a> in <a--}}
-    {{--                        href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
-    {{--            </p>--}}
-
-    {{--            <div>{!! $post->excerpt !!} </div>--}}
-    {{--        </article>--}}
-    {{--    @endforeach--}}
-
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
+        @if($posts->count() > 0)
+            <x-post-featured-card :post="$posts[0]"/>
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+            @if($posts->count() > 1)
+                <x-posts-grid :posts="$posts"/>
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+            @endif
+        @else
+            <p class="text-center"> No post yet. Please check back later</p>
+        @endif
     </main>
 </x-layout>
