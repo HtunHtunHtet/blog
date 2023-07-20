4403fcae9d139613916a467cd236b05c6784b0f1
htunhtunhtet <ryanhhh91@gmail.com>
2023-07-20 22:27:19 +0800

    Add comment layout

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 rounded=xl border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img class="rounded-xl" src="https://i.pravatar.cc/100" alt="user-avatar" width="60"
+             height="60"/>
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Dohe</h3>
+            <p class="text-xs">Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            Earum reprehenderit perferendis nisi commodi. Eius quo eos enim sequi voluptatem quia.
+            Commodi mollitia pariatur neque molestiae quam delectus earum.
+        </p>
+    </div>
+</article>
