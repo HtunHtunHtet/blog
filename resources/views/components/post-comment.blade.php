9e66891826c4894349eb21771fc78aaea0bfbfaf
htunhtunhtet <ryanhhh91@gmail.com>
2023-07-20 23:49:20 +0800

    Add dynamic post from db

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,20 +1,21 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 p-6 rounded=xl border border-gray-200 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/100" alt="user-avatar" width="60"
+        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="user-avatar" width="60"
              height="60"/>
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Dohe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
         <p>
-            Earum reprehenderit perferendis nisi commodi. Eius quo eos enim sequi voluptatem quia.
-            Commodi mollitia pariatur neque molestiae quam delectus earum.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
