1d596902d86cd2b855394366d61f8e1dbe518c23
htunhtunhtet <ryanhhh91@gmail.com>
2023-07-28 00:33:27 +0800

    Add settings

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,6 +1,6 @@
 @props(['trigger'])
 
-<div x-data="{ show: false}" @click.away="show = false">
+<div x-data="{ show: false}" @click.away="show = false" class="relative">
     {{-- Trigger--}}
     <div @click="show = !show">
         {{ $trigger }}
@@ -10,6 +10,5 @@
     <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50 overflow-auto max-h-52"
          style="display:none;">
         {{ $slot }}
-
     </div>
 </div>
